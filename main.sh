efd3c33d83271a60d03541865bf40a511b70dd4c
Antoine Sirot <101109062+AntoineSirot@users.noreply.github.com>
2022-11-30 15:55:06 +0100

    Rendu Final

--- main.sh
+++ main.sh
@@ -4,7 +4,8 @@ DAY=1440 # Time between every daily report (in minute here)
 HOUR=60 # Time between every message with the price (in minute here)
 PAUSE=60 # Time between every scrapping
 COMPT=0 # Number of printed values
-declare -a VALUES # Array that will stack prices of Bitcoin 
+declare -a VALUES # Array that will stack prices of Bitcoin and reset everyday
+declare -a PRICES # Array that will stack every price of Bitcoin since the launching
 
 # The next 3 lines initilalize values
 TOTAL=0
@@ -18,7 +19,7 @@ curl -s https://cryptorank.io/price/bitcoin > cryptorank.txt # Scrapping Bitcoin
 # The next 3 lines delete the ',' between Bitcoin Values (ex : 16,123 => 16123)
 PRICE=$(cat cryptorank.txt | grep -oP '<div class="table-cell-with-update__CurrencyWrapper-sc-crypsw-0 iJwtNA">\$ \w+,\w+<' | grep -oP '\w+,\w+')
 TAMP=$(echo $PRICE | sed 's/,//')
-PRICE=$(($TAMP))
+PRICE=$(($TAMP)) 
 
 echo "BITCOIN's price, printing number $(($COMPT+1)) : \$$PRICE" # You can mute this line if you don't want any print
 
@@ -26,8 +27,8 @@ COMPT=$(( $COMPT + 1 )) # Incrementation of COMPT
 
 sleep $PAUSE # Gives us the price every minute
 
-VALUES[$(($COMPT))]=$PRICE # Keep every value that will be necessary for total, mean, max and min
-
+VALUES[$(($COMPT))]=$PRICE # Keep every value that will be necessary for total, mean, max and min (reset everyday)
+PRICES[$(($COMPT + $NUMBER_DAYS * $DAY ))]=$PRICE # Keep every values 
 
 if (($COMPT==1)); then
     OPEN_PRICE=$PRICE # Taking the first price for the Open Price
@@ -73,32 +74,33 @@ your_chat_id="YOUR_CHAT_ID"
 your_telegram_token="YOUR_TELEGRAM_TOKEN"
 
 if (( $(($COMPT % $HOUR)) == 0 )); then # 60 min = 1 hour
-curl --data  chat_id=$your_chat_id --data-urlencode "text= Bitcoin's price now  : \$${PRICE} !" "https://api.telegram.org/bot$your_telegram_to
-ken/sendMessage?parse_mode=HTML" # Sending a Telegram message with current Bitcoin Price, its Max, Min and Mean of the last 24 hours 
-echo "" # Printing nothing so next print will be on a new line
+        time=$(date) # Adding the date for the message
+        curl --data  chat_id=$your_chat_id --data-urlencode "text= $time
+Bitcoin's price now  : \$${PRICE} !" "https://api.telegram.org/bot$your_telegram_token/sendMessage?parse_mode=HTML" # Sending a Telegram message with current Bitcoin Price, its Max, Min and Mean of the last 24 hours 
+        echo "" # Printing nothing so next print will be on a new line
 fi
 
 if (( $COMPT == $DAY )); then # 1440 min = 1 day 
-curl --data  chat_id=$your_chat_id --data-urlencode "text= Bitcoin's price now  : \$${PRICE} 
+        time=$(date) # Adding the date for the message
+        curl --data  chat_id=$your_chat_id --data-urlencode "text= $time
+Bitcoin's price now  : \$${PRICE} 
 Mean in the last 24h :\$${MEAN}
 Open Price : \$${OPEN_PRICE}
 Close Price : \$${CLOSE_PRICE}
 Lower price : \$${min}
 Higher price : \$${max}
 Maximum gap : \$${MAX_GAP}
-Evolution in the last 24h : ${DAILY_GAP}% " "https://api.telegram.org/bot$your_telegram_token/sendMessage?parse_mode=HTML" # Sending a Telegram messag
-e with current Bitcoin Price, its Max, Min and Mean of the last 24 hours 
-echo "" # Printing nothing so next print will be on a new line
-COMPT=0 # Restart for next day
-unset VALUES # Restart for next day
-declare -a VALUES # Restart for next day
+Evolution in the last 24h : ${DAILY_GAP}% " "https://api.telegram.org/bot$your_telegram_token/sendMessage?parse_mode=HTML" # Sending a Telegram message with current Bitcoin Price, its Max, Min and Mean of the last 24 hours 
+        echo "" # Printing nothing so next print will be on a new line
+        COMPT=0 # Restart for next day
+        unset VALUES # Restart for next day
+        declare -a VALUES # Restart for next day
+        # The next 3 lines reinitilalize values
+        TOTAL=0
+        min=1000000
+        max=0
 
-# The next 3 lines reinitilalize values
-TOTAL=0
-min=1000000
-max=0
 fi
 
 done
 
-exit
